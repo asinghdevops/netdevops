dde64f45f3c2f6e27ca62b23146d0c4296f82451
asinghdevops <63793375+asinghdevops@users.noreply.github.com>
2021-08-14 19:07:48 +0200

    Update show_version.py

--- show_version.py
+++ show_version.py
@@ -1,4 +1,4 @@
-# Python NEtmiko script to run IOS command on CSR1000v Router
+# Python Netmiko script to run IOS command on CSR1000v Router
 
 import netmiko
 connection = netmiko.ConnectHandler(ip="10.10.20.48", device_type="cisco_ios", username="developer", password="C1sco12345")
